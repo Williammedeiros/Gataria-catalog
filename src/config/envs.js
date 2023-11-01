5b4b07f46ce06bb58d382c2fe48e79f17142f71f
William Medeiros <william.medeiros007@gmail.com>
2023-11-01 19:43:57 -0300

    first commit

--- src/config/envs.js
+++ src/config/envs.js
@@ -1,2 +1,2 @@
-export const mongoURL = process.env.MONGO_URL;
-export const mongoDatabase = process.env.MONGO_DATABASE;
+export const mongoURL = "mongodb+srv://will:will210176@dev.clfbdok.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp";
+export const mongoDatabase = "dev";
